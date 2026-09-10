236604966c54f2740570a1b741ca06bb919a37bc
Muhamad Raka Pratama <raka824pratama@gmail.com>
2026-09-10 22:50:56 +0700

    Mengubah href pada Logo

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <MainLayout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
+          <Route path="/produk" element={<Product />} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
