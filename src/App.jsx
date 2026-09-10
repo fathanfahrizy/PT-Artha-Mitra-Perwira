de88b4c9c6b26bc19cba6d6a656216d47c03017b
Muhamad Raka Pratama <raka824pratama@gmail.com>
2026-09-10 14:26:24 +0700

    Menambahkan React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/templates/MainLayout';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <MainLayout>
-      <Home />
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
